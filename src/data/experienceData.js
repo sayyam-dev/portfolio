d1452817415001a1775d0216439f0f1f4d37062d
sayyam-dev <ms2546983@gmail.com>
2024-09-30 21:22:27 -0700

    hoping for deploy

---
+++ src/data/experienceData.js
@@ -0,0 +1,44 @@
+const expData = [
+   
+    {
+      id: "1",
+      org: "AGT ",
+      title: "Web Developer",
+      date: "5-Mar-2023 to 6-Sep-2023",
+      description:  "A passionate web designer with expertise in HTML5, CSS, and Bootstrap. I hold a Web Design Certification from AGT Institute, Saddar, completed in September 2023, and gained practical experience working at AGT Saddar Software House. My skills focus on building responsive, visually appealing websites. You can verify my certification https://www.agt.edu.pk/online/verification."
+    },
+    {
+        id: "2",
+        org: "EMTS Company",
+        title: "Junior Computer Operator",
+        date: "5-April-2023 to  10-Jan-2024",
+        description:  "I have experience as a Junior Computer Operator where I handled data entry, digital records management, and provided technical support to ensure smooth operations. My skills in computer applications and problem-solving helped me efficiently manage both IT and administrative tasks. This role strengthened my ability to work under pressure and deliver quality outcomes.."
+      },  
+    {
+        id: "3",
+        org: "Infrolernce ",
+        title: "Full Stack Developer",
+        date: "Jan 2023 to Oct-2023",
+        description:  
+            "I am Sayyam Shahid, with experience in front-end development using HTML, CSS, JavaScript, Bootstrap, and TypeScript to create responsive and scalable applications. On the back end, I work with PHP, MySQL, Node.js, and Express.js for server-side programming and API development. I am proficient in full-stack development with the MERN stack (MongoDB, Express, React, Node.js), and I have expertise in React.js and Next.js for building dynamic, SEO-friendly web apps. Additionally, I am skilled in Python, C, and C++ for scripting and system-level programming.",
+      },   
+    {
+        id: "4",
+        org: "Sky Plus Solutions Pvt.Ltd ",
+        title: "ReactJS/NextJS Developer | Full Stack Developer",
+        date: "Since June-2024",
+        description:  
+            "I'm Sayyam Shahid, currently interning as a full-stack developer at The Sky Coaching Software House. My work spans frontend development, where I create responsive user interfaces using React and Next.js, and backend development, where I handle API integration, Node.js, Express.js, and MongoDB. I've had the opportunity to work on real-life projects, contributing to both the technical implementation and the creation of Software Requirements Specifications (SRS). This hands-on experience has been invaluable, enhancing my skills and preparing me to tackle complex challenges in software development. I'm passionate about learning and continuously improving in this field.",
+      },  
+    {
+        id: "4",
+        org: "Freelance",
+        title: "MERN/ReactJS/NextJS Developer/HTML,CSS,JS",
+        date: "Since 2023",
+        description:  
+            "I am Sayyam Shahid, and I work as a freelancer on Fiverr and with local clients, creating websites using HTML, CSS, JavaScript, Bootstrap, and TypeScript. I also handle back-end development with PHP, MySQL, Node.js, and Express.js, and excel in full-stack development with the MERN stack. Additionally, I am skilled in React.js and Next.js for building dynamic, SEO-friendly web apps. Currently, I am working on real-time projects like the Emmoeence website.",
+      },  
+    
+];
+
+export default expData;
